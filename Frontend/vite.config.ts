7895dc7a44c9318702617495a79d3aaf97af8e0c
Masfik Talukdar <freelancermasfik@gmail.com>
2025-10-21 14:25:07 +0600

    Store added

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
 			"@hooks": path.resolve(__dirname, "./src/hooks"),
 			"@lib": path.resolve(__dirname, "./src/lib"),
 			"@pages": path.resolve(__dirname, "./src/pages"),
+      "@store": path.resolve(__dirname, "./src/store"),
 			"@styles": path.resolve(__dirname, "./src/styles"),
 			"@app-types": path.resolve(__dirname, "./src/types"),
 			"@utils": path.resolve(__dirname, "./src/utils"),

b39234121bf24324d1cdd08eb70b13d03c134e2b
Masfik Talukdar <freelancermasfik@gmail.com>
2025-10-27 15:30:57 +0600

    Creating a blnking button for Admin Station and admin Station store in zustand

--- Frontend/src/components/button.tsx
+++ Frontend/src/components/button.tsx
@@ -1,13 +1,36 @@
-import React from "react"
+import "@styles/blinkingButtonStyle.css";
 
-interface ButtonProps {
-  children: React.ReactNode, 
-  onClick: ()=> void
+interface PrimaryButtonProps {
+	children: React.ReactNode;
+	onClick: () => void;
 }
 
-
-export const ButtonPrimary = function({children, onClick}: ButtonProps){
-  return(
-    <div className="flex justify-center items-center bg-primary-800 h-auto w-auto px-6 py-2 text-white font-mono font-medium text-lg rounded-[4px] cursor-pointer" onClick={onClick}>{children}</div>
-  )
+interface BlinkingButtonProp {
+	onClick: () => void;
 }
+
+export const ButtonPrimary = function ({
+	children,
+	onClick,
+}: PrimaryButtonProps) {
+	return (
+		<div
+			className="flex justify-center items-center bg-primary-800 h-auto w-auto px-6 py-2 text-white font-mono font-medium text-lg rounded-[4px] cursor-pointer"
+			onClick={onClick}
+		>
+			{children}
+		</div>
+	);
+};
+
+export const RoundedBlinkingButton = function ({
+	onClick,
+}: BlinkingButtonProp) {
+	return (
+    <div className="pulse-btn-div">
+      <button className="pulse-btn" onClick={onClick}>
+        Create New <br/> Route
+      </button>
+    </div>
+	);
+};
--- Frontend/src/features/admin/pages/adminStationRoutes.tsx
+++ Frontend/src/features/admin/pages/adminStationRoutes.tsx
@@ -1,9 +1,14 @@
+import { RoundedBlinkingButton } from "@/components/button"
 import Footer from "@/components/footer"
+
+function blinkingBtnAction(){
+  console.log("everyting is working")
+}
 
 export default function AdminStationRoutes(){
   return(
     <div className="w-full min-h-full bg-primary-100 flex-1 flex flex-col">
-
+        <RoundedBlinkingButton onClick={blinkingBtnAction}/>
       {/* Footer Section */}
       <Footer/>
     </div>
